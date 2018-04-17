dddf0168bea5c30f801fd560513e03f55de29724
ohduran <alvaro.duranb@gmail.com>
2018-04-17 07:39:35 +0100

    Progress defining LongDecimal class

--- new version/longdecimals.py
+++ new version/longdecimals.py
@@ -6,8 +6,19 @@ class LongDecimal():
 
     def __init__(self, ciphers=[0], negative=False):
         """Define constructor method."""
-        self._negative = negative
-        self._ciphers = ciphers
+        if not isinstance(ciphers, list):
+            raise Exception('The parameter ciphers must be a list.')
+        if any([not isinstance(cipher, int) for cipher in ciphers]):
+            raise Exception('Some of the items in "ciphers" are not integers.')
+        if not isinstance(negative, bool):
+            raise Exception('The parameter negative must be a boolean.')
+
+        if any([cipher < 0 for cipher in ciphers]):
+            self._negative = True
+        else:
+            self._negative = negative
+
+        self._ciphers = [abs(cipher) for cipher in ciphers]
 
     def nodecimals(self):
         """Return number of decimals."""
@@ -16,6 +27,18 @@ class LongDecimal():
     def iszero(self):
         """Return True if all ciphers are zero."""
         return all(cipher == 0 for cipher in self._ciphers)
+
+    def isnegative(self):
+        """Return True if negative."""
+        return self._negative
+
+    def carryover(self):
+        """Ammends the ciphers to ensure all of them fall in [0,10]."""
+        for i in range(1, self.nodecimals + 1):
+            tens = self._ciphers[i] // 10
+            if tens > 0:
+                self._ciphers[i] -= 10 * tens
+                self._ciphers[i - 1] += 10
 
     def __repr__(self):
         """Print LongDecimal instance."""
--- new version/tests.py
+++ new version/tests.py
@@ -1,16 +1,86 @@
 import unittest
+import random
 from longdecimals import LongDecimal
+
+
+def ciphers_generator():
+        """Generate a valid ciphers list of up to 9 decimals."""
+        return [random.randint(0, 9) for i in range(random.randint(1, 10))]
 
 
 class TestLongDecimal(unittest.TestCase):
     """Test LongDecimal class properties."""
+
+    def test_iszero_method(self):
+        """Test iszero method."""
+        ciphers = ciphers_generator()
+        ld = LongDecimal(ciphers=ciphers)
+        zero = LongDecimal(ciphers=[0])
+        zerodefault = LongDecimal()
+
+        self.assertFalse(ld.iszero())
+        self.assertTrue(zero.iszero())
+        self.assertTrue(zerodefault.iszero())
+
+    def test_nodecimals_method(self):
+        """Test nodecimals method."""
+        ciphers = ciphers_generator()
+        ld = LongDecimal(ciphers=ciphers)
+
+        expected_result = len(ciphers) - 1
+        self.assertEqual(ld.nodecimals(), expected_result)
 
     def test_repr_method(self):
         """Test __repr__ method."""
         ciphers = [1, 2, 3]
         ld = LongDecimal(ciphers=ciphers)
         expected_result = '1.23'
-        self.assertEqual(ld.__repr__, expected_result)
+        self.assertEqual(ld.__repr__(), expected_result)
+
+    def test_isnegative(self):
+        """Test isnegative method."""
+        ciphers = ciphers_generator()
+        ld = LongDecimal(ciphers=ciphers, negative=True)
+
+        self.assertTrue(ld.isnegative())
+
+    def test_negative_ciphers_negative_by_default(self):
+            """
+            Long Decimal must be negative by default
+            if any cipher in ciphers is negative.
+            """
+            ciphers_1 = [-1, 2, 3]
+            ciphers_2 = [1, -2, 3]
+            ld1 = LongDecimal(ciphers=ciphers_1)
+            ld2 = LongDecimal(ciphers=ciphers_2)
+
+            self.assertTrue(ld1.isnegative())
+            self.assertTrue(ld2.isnegative())
+
+    def test_negative_ciphers_turn_all_positive(self):
+            """Long Decimal must return all ciphers positive."""
+            ciphers_1 = [-1, 2, 3]
+            ciphers_2 = [1, -2, 3]
+            expected_ciphers_stored = [1, 2, 3]
+            ld1 = LongDecimal(ciphers=ciphers_1)
+            ld2 = LongDecimal(ciphers=ciphers_2)
+
+            self.assertEqual(ld1._ciphers, expected_ciphers_stored)
+            self.assertEqual(ld2._ciphers, expected_ciphers_stored)
+
+    def test_exception_raised_when_ciphers_not_a_list(self):
+            """An Exception is raised if any cipher is not a list."""
+            invalid_ciphers = 12
+
+            with self.assertRaises(Exception):
+                    LongDecimal(ciphers=invalid_ciphers)
+
+    def test_exception_raised_when_ciphers_not_int(self):
+            """An Exception is raised if any cipher is not int."""
+            invalid_ciphers = [1, 2, 3.3]
+
+            with self.assertRaises(Exception):
+                    LongDecimal(ciphers=invalid_ciphers)
 
 
 if __name__ == '__main__':
